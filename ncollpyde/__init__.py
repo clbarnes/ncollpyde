350c081d9dc298272313a8ea08013fec5f6f1ea4
Chris Barnes <barnesc@janelia.hhmi.org>
2020-05-22 13:45:38 +0100

    Make version split more flexible

--- ncollpyde/__init__.py
+++ ncollpyde/__init__.py
@@ -9,6 +9,6 @@ from .ncollpyde import _version
 from .main import Volume
 
 __version__ = _version()
-__version_info__ = tuple(int(n) for n in __version__.split("."))
+__version_info__ = tuple(int(n) for n in __version__.split("-")[0].split("."))
 
 __all__ = ["Volume"]
