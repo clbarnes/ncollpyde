aef3891871382030ab552629d30ecf276a6c40ab
Chris Barnes <barnesc@janelia.hhmi.org>
2020-11-30 14:57:16 +0000

    expose constants in module

--- ncollpyde/__init__.py
+++ ncollpyde/__init__.py
@@ -6,7 +6,14 @@ __author__ = """Chris L. Barnes"""
 __email__ = "chrislloydbarnes@gmail.com"
 
 from .ncollpyde import _version
-from .main import Volume
+from .main import (  # noqa: F401
+    Volume,
+    N_CPUS,  # noqa: F401
+    DEFAULT_THREADS,  # noqa: F401
+    DEFAULT_RAYS,  # noqa: F401
+    DEFAULT_SEED,  # noqa: F401
+    PRECISION,  # noqa: F401
+)
 
 __version__ = _version()
 __version_info__ = tuple(int(n) for n in __version__.split("-")[0].split("."))
